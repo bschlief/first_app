71cd26742cb1be203d824f5b8f8f9e1c84392775
Bryan Schlief <bschlief@gmail.com>
2012-09-03 19:17:53 -0700

    Rails Tutorial - Create new app

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '31ffc5a52ebeabc5f3b814c3d44a15d76652e58055520eb304181f793bddf733f6736914d2b2710f479519b3a641c9a48762d7592e1e3a27b258fde46b9df06d'
